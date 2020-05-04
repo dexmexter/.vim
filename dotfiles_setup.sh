6b4beea9efa3d37c8b40cda71bde3404b3e5058d
dexmexter <dexter.volkman@protonmail.com>
2020-05-03 19:13:10 -0700

    add verbose commands, remove echo statements

--- dotfiles_setup.sh
+++ dotfiles_setup.sh
@@ -2,22 +2,17 @@
 
 # symlink for neovim config if neovim installed, else create vimrc symlink
 if command -v nvim >/dev/null 2>&1; then
-    mkdir -p ~/.config/nvim/
-    ln -si ~/.dotfiles/vimrc ~/.config/nvim/init.vim &&
-    echo "vimrc installed at ~/.config/nvim/init.vim"
+    mkdir -vp ~/.config/nvim/
+    ln -vsi ~/.dotfiles/vimrc ~/.config/nvim/init.vim
 else
-    mkdir -p ~/.vim/
-    ln -si ~/.dotfiles/vimrc ~/.vim/vimrc &&
-    echo "vimrc installed at ~/.vim/vimrc"
+    mkdir -vp ~/.vim/
+    ln -vsi ~/.dotfiles/vimrc ~/.vim/vimrc
 fi
 
-ln -si ~/.dotfiles/bashrc ~/.bashrc &&
-echo "bashrc installed at ~/.bashrc"
+ln -vsi ~/.dotfiles/bashrc ~/.bashrc
 
-mkdir -p ~/.config/tmux/
-ln -si ~/.dotfiles/tmux.conf ~/.config/tmux/tmux.conf &&
-echo "tmux.conf installed at ~/.config/tmux/tmux.conf"
+mkdir -vp ~/.config/tmux/
+ln -vsi ~/.dotfiles/tmux.conf ~/.config/tmux/tmux.conf
 
-mkdir -p ~/.config/git/
-ln -si ~/.dotfiles/gitconfig ~/.config/git/config &&
-echo "gitconfig installed at ~/.config/git/config"
+mkdir -vp ~/.config/git/
+ln -vsi ~/.dotfiles/gitconfig ~/.config/git/config
