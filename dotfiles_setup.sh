df37777c2f4477ba6ae14d8bfb96a0b0b61020b1
dexmexter <dexter.volkman@protonmail.com>
2020-07-01 21:48:30 -0700

    relocate dotfiles folder to .config

--- dotfiles_setup.sh
+++ dotfiles_setup.sh
@@ -3,16 +3,16 @@
 # symlink for neovim config if neovim installed, else create vimrc symlink
 if command -v nvim >/dev/null 2>&1; then
     mkdir -vp ~/.config/nvim/
-    ln -vsi ~/.dotfiles/vimrc ~/.config/nvim/init.vim
+    ln -vsi ~/.config/dotfiles/vimrc ~/.config/nvim/init.vim
 else
     mkdir -vp ~/.vim/
-    ln -vsi ~/.dotfiles/vimrc ~/.vim/vimrc
+    ln -vsi ~/.config/dotfiles/vimrc ~/.vim/vimrc
 fi
 
-ln -vsi ~/.dotfiles/bashrc ~/.bashrc
+ln -vsi ~/.config/dotfiles/bashrc ~/.bashrc
 
 mkdir -vp ~/.config/tmux/
-ln -vsi ~/.dotfiles/tmux.conf ~/.config/tmux/tmux.conf
+ln -vsi ~/.config/dotfiles/tmux.conf ~/.config/tmux/tmux.conf
 
 mkdir -vp ~/.config/git/
-ln -vsi ~/.dotfiles/gitconfig ~/.config/git/config
+ln -vsi ~/.config/dotfiles/gitconfig ~/.config/git/config
